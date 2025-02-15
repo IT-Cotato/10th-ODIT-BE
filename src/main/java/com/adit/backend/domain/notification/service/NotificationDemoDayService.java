424fdf03c5c273d1b7119047cd34ef8f7d71583b
supsup_hae <zksdbstjq@gmail.com>
2025-02-15 22:28:27 +0900

    :egg: feat: Easter Bunny!! 🐰

---
+++ src/main/java/com/adit/backend/domain/notification/service/NotificationDemoDayService.java
@@ -0,0 +1,71 @@
+package com.adit.backend.domain.notification.service;
+
+import org.springframework.stereotype.Service;
+
+import com.adit.backend.domain.event.entity.UserEvent;
+import com.adit.backend.domain.event.exception.EventException;
+import com.adit.backend.domain.event.repository.UserEventRepository;
+import com.adit.backend.domain.notification.converter.NotificationEventConverter;
+import com.adit.backend.domain.notification.event.NotificationEvent;
+import com.adit.backend.domain.notification.service.command.NotificationCommandService;
+import com.adit.backend.domain.place.entity.UserPlace;
+import com.adit.backend.domain.place.exception.PlaceException;
+import com.adit.backend.domain.place.repository.UserPlaceRepository;
+import com.adit.backend.domain.place.service.query.UserPlaceQueryService;
+import com.adit.backend.global.error.GlobalErrorCode;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 데모데이를 위한 서비스입니다 😅
+ */
+@Slf4j
+@Service
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+public class NotificationDemoDayService {
+	private final NotificationEventConverter eventConverter;
+	private final UserPlaceQueryService userPlaceQueryService;
+	private final UserPlaceRepository userPlaceRepository;
+	private final NotificationCommandService notificationCommandService;
+	private final UserEventRepository userEventRepository;
+
+	/**
+	 *  원영이형한테 기간입력하라고 알림보내기
+	 *  1. 원영이형의 유저 ID == 1
+	 *  2. 기간 미입력된 이벤트를 ID 1 로 정의
+	 */
+	public void sendDurationNotification() {
+		UserEvent userEvent = userEventRepository.findById(1L)
+			.orElseThrow(() -> new EventException(GlobalErrorCode.EVENT_NOT_FOUND));
+		NotificationEvent event = eventConverter.toMissingEvent(userEvent);
+		notificationCommandService.sendNotification(event);
+	}
+
+	/**
+	 *  원영이형한테 방문하라고 알림보내기
+	 *  1. 원영이형의 유저 ID == 1
+	 *  2. 알림을 보내고싶은 임의의 장소를 ID 1 로 정의
+	 */
+	public void sendUnvisitedNotification() {
+		UserPlace userPlace = userPlaceRepository.findById(1L)
+			.orElseThrow(() -> new PlaceException(GlobalErrorCode.USER_PLACE_NOT_FOUND));
+		NotificationEvent event = eventConverter.toUnvisitedEvent(userPlace);
+		notificationCommandService.sendNotification(event);
+	}
+
+
+	/**
+	 *  원영이형한테 방문하라고 알림보내기
+	 *  1. 원영이형의 유저 ID == 1
+	 *  2. 알림을 보내고싶은 임의의 장소를 시작 일이 데모데이 기준 3일 남도록 데이터 저장
+	 */
+	public void sendStartNotification() {
+		UserEvent userEvent = userEventRepository.findById(1L)
+			.orElseThrow(() -> new EventException(GlobalErrorCode.EVENT_NOT_FOUND));
+		NotificationEvent event = eventConverter.toStartEvent(userEvent, 3);
+		notificationCommandService.sendNotification(event);
+	}
+
+}
